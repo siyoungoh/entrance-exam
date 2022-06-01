de24ae4b87cbc4bc97861f17929f0bc84f19f4a0
siyongoh <dev@syoh.net>
2022-06-01 12:57:11 +0900

    추가 - 요구사항 C 구현-버킷리스트 프로젝트 방식

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import pymongo
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 
@@ -18,9 +19,18 @@ def save_attraction():
     attraction = request.form["attraction"]
     description = request.form["description"]
 
+    attractions_list = list(db.attractions.find({}, {'_id': False}))
+    previous_id = len(attractions_list)
+    if previous_id > 0:
+        attraction_id = previous_id + 1
+    else:
+        attraction_id = 1
+
     doc = {
+        "attractionId": attraction_id,
         "attraction": attraction,
         "description": description,
+        "read": False,
     }
     db.attractions.insert_one(doc)
     return jsonify({"status": "saved"})
@@ -28,8 +38,18 @@ def save_attraction():
 
 @app.route("/attractions", methods=["GET"])
 def get_attractions():
-    attractions = list(db.attractions.find({}, {"_id": False}))
+    attractions = list(db.attractions.find({}, {"_id": False}).sort("read", pymongo.ASCENDING))
+    # 참고 문서
+    # https://pymongo.readthedocs.io/en/stable/api/pymongo/cursor.html#pymongo.cursor.Cursor.sort
+    # https://pymongo.readthedocs.io/en/stable/api/pymongo/collection.html#pymongo.ASCENDING
     return jsonify({"attractions": attractions})
+
+
+@app.route("/attraction/read", methods=["POST"])
+def mark_as_read():
+    attraction_id = request.form["attractionId"]
+    db.attractions.update_one({"attractionId": int(attraction_id)}, {"$set": {"read": True}})
+    return jsonify({"status": "marked as read"})
 
 
 if __name__ == "__main__":
